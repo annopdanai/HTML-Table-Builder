6e40eaacb708a83b386fc4d1b08c23dc6c26c9e0
beam2546 <beam2546@users.noreply.github.com>
2021-12-14 01:47:40 +0700

    Align text and table to center + Decrease width to 50%

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char ch;
+char ch[10];
 
 int main() {
     int column, row;
@@ -15,26 +15,15 @@ int main() {
     scanf("%d", &row);
     printf("Enter the number of columns: ");
     scanf("%d", &column);
-<<<<<<< Updated upstream
-    fprintf(html, "<html>\n<body>\n<table border=\"1\">\n");
-=======
     fprintf(html, "<html>\n<body>\n<center>\n<table border=\"1\" style=\"width:50%%\">\n");
->>>>>>> Stashed changes
     for(int i = 0; i < column; i++)
     {
-        fprintf(html, "<tr>\n");
+        fprintf(html, "<tr style=\"height:50px\">\n");
         for(int j = 0; j < row; j++)
         {
-<<<<<<< Updated upstream
-            while ((ch = getchar()) != '\n' && ch != EOF);
-            printf("Enter the value of the cell %d%d: ", j + 1, i + 1);
-            scanf("%d", &ch);
-            fprintf(html, "<td>%d</td>\n", ch);
-=======
             printf("Enter the value of the cell %d - %d: ", i + 1, j + 1);
             scanf("%s", ch);
             fprintf(html, "<td style=\"text-align: center; vertical-align: middle;\">%s</td>\n", ch);
->>>>>>> Stashed changes
         }
         fprintf(html, "</tr>\n");
     }
