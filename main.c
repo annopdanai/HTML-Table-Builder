7920998f1efc3bbc336bf312f4fb18d5e7f8d44e
beam2546 <beam2546@users.noreply.github.com>
2021-12-13 22:52:24 +0700

    Finished Basic HTML Table Builder

--- main.c
+++ main.c
@@ -13,6 +13,16 @@ int main() {
     scanf("%d",&cell);
     printf("Number of row: ");
     scanf("%d",&row);
-    //code goes here
+    fprintf(html, "<table border=\"1\">\n");
+    for(int i = 0; i < row; i++)
+    {
+        fprintf(html, "<tr>\n");
+        for(int j = 0; j < cell; j++)
+        {
+            fprintf(html, "<td>%d</td>\n", i*cell+j+1);
+        }
+        fprintf(html, "</tr>\n");
+    }
+    fprintf(html, "</table>\n");
     fclose(html);
 }
